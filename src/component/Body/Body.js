35feee228be3553ef8c51d41cd1da9ab3cd5284d
ahmadijmail <ahmadijmil2010@gmail.com>
2022-09-12 16:02:10 +0300

    first commit

---
+++ src/component/Body/Body.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { When } from "react-if";
+import './body.scss'
+const Body = () => {
+    const [imei, setimei] = useState({ type: "" });
+    const [serviceInfo, setserviceInfo] = useState({ type: "" });
+    const [serviceid, setserviceID] = useState({ type: "" });
+    const [result, setresult] = useState();
+    let url = process.env.REACT_APP_API_URL;
+    let key = process.env.REACT_APP_API_KEY;
+  
+    const imeicCheck = async (e) => {
+      e.preventDefault();
+  
+      axios
+        .post(url, null, {
+          params: {
+            service: serviceInfo.id,
+            imei: imei.imei,
+            key: key,
+          },
+        })
+        .then((res) => {
+          setresult({ response: res.data });
+          setserviceID(res.config.params.service);
+          console.log("API Response", res);
+        })
+        .catch((error) => {
+          setresult(error);
+        });
+    };
+  return (
+   <>
+    <select
+        onChange={(e) => {
+          const ServiceName = e.target.value;
+          console.log(ServiceName);
+
+          if (e.target.value == "FindMy") {
+            setserviceInfo({ id: 4 });
+          } else if (ServiceName == "AppleBasicInfo") {
+            setserviceInfo({ id: 205 });
+          }
+          console.log("serviceInfo", serviceInfo);
+        }}
+        className="btn2"
+        id="copy"
+      >
+        <option className="btn" value="">
+          Select Service
+        </option>
+
+        <option value="FindMy" className="submit">
+          Find My iPhone
+        </option>
+
+        <option value="AppleBasicInfo" className="submit">
+          Apple Basic info
+        </option>
+      </select>
+
+      <div className="buttonIn">
+        <input
+          className="btn3"
+          type="text"
+          placeholder="IMEI / Serial"
+          name="imei"
+          onChange={(e) => setimei({ imei: e.target.value })}
+        />
+        <button className="btn4" onClick={imeicCheck}>
+          submit
+        </button>
+      </div>
+
+      {serviceid == 4 ? (
+        <div>
+          <div className="courses-container">
+            <div className="course">
+              <div className="course-preview">
+                <div className="course-info">
+                  <img
+                    className="img"
+                    src="https://support.apple.com/library/content/dam/edam/applecare/images/en_US/iOS/ios13-iphone-xs-home-assistive-touch.png"
+                  />
+                </div>
+              </div>
+              <div className="course-info">
+                <h2> IMEI: {result.response.object.imei}</h2>
+
+                <h4>
+                  {" "}
+                  Find My iPhone:{" "}
+                  {result.response.object.fmiON == false ? "OFF" : "ON"}
+                  <span>{result ? result.response.name : ""}</span>
+                </h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+
+      {serviceid == 205 ? (
+        <div className="courses-container">
+          <div className="course">
+            <div className="course-preview">
+              <h6>Service: Basic info 0.5$</h6>
+              <td width="80">
+                <img className="img" src={result.response.object.thumbnail} />
+              </td>{" "}
+              <br />
+              <a href="#">
+                View All Details <i class="fas fa-chevron-right"></i>
+              </a>
+            </div>
+            <div className="course-info">
+              <h2>{result.response.object.modelDesc}</h2>
+              <h4>Serial : {result.response.object.serial}</h4>
+              <h4>
+                Purchase Country: {result.response.object.purchaseCountry}{" "}
+              </h4>
+              <h4>Purchase Date: {result.response.object.estPurchaseDate}</h4>
+              <h4>Warranty Status: {result.response.object.warrantyStatus}</h4>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+   </>
+  )
+}
+
+export default Body
---
+++ src/component/NavBar/NavBar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './navBar.scss'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavBar = () => {
+  
+  return (
+    <div>
+    
+    <nav class="navbar navbar-dark bg-dark">
+
+
+
+      <br />
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
